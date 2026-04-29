28bd6851af38c3e94eed9153ee46df2d1ccd8e48
Abhi <abhishek.mishra.dev@hotmail.com>
2026-04-29 16:51:11 -0400

    Change input event logging to alert value

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const el = document.getElementById('identifier-field');
  
 // Log every change
-el.addEventListener('input', (e) => console.log('value:', e.target.value));
+el.addEventListener('input', (e) => alert(e.target.value));
